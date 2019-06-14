a86c461f8655b92323d8b4aa8d17bad38e6ceb6d
frankast <borodaev97@gmail.com>
2019-06-14 14:46:01 +0600

    feat(routes.rb): set up app routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user/show'
+  get 'auth/new'
+  get 'auth/create'
+  get 'auth/destroy'
+  get 'auth/callback'
+
+  root 'auth#new'
 end
